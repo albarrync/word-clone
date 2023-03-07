64a132035b2ab0cd2176d4cdd74d594549d39534
alexb <albarrync@gmail.com>
2023-03-07 18:43:42 -0500

    Exercise 3 of Word Clone Project
    Acceptance Criteria:
    
    * Create new Guess component. 6 instances should be rendered
    * Guess component should render 5 spans with class of cell
    * Each cell should contain a letter from the appropriate guess
    * Use the NUM_OF_GUESSES_ALLOWED constant
    * No key warnings

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Guess from '../Guess';
 import GuessInput from '../GuessInput';
 
-import { sample } from '../../utils';
+import { sample, range } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -22,8 +23,11 @@ function Game() {
   }
 
   return <>
+
     <div className="guess-results">
-      {guessList.map((text, index) => <Guess guessString={text} key={index} />)}
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((guessRow, index) => {
+        return (<Guess key={index} guessString={ guessList[index] } /> )
+      })}
     </div>
 
     <GuessInput handleGuess={handleGuess}
